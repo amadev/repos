e77eeff8d2d2dd259c62d6d5fd7d28361500cf8c
Andrey Volkov <avolkov@mirantis.com>
2019-06-29 01:16:01 +0300

    Initial install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o errexit
+
+__DIR__="$(cd "$(dirname "${0}")"; echo $(pwd))"
+__BASE__="$(basename "${0}")"
+__FILE__="${__DIR__}/${__BASE__}"
+
+# mkdir -p ~/.repos/
+# cat << 'EOF' > ~/.repos/db.yaml
+# repos: []
+# groups: []
+# EOF
+
+cat << EOF > /usr/local/bin/repos
+#!/usr/bin/env bash
+
+$__DIR__/repos
+EOF
+chmod 755 /usr/local/bin/repos

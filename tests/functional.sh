90f73dac81326f7b15ea97d7fb611a1c79da3895
Andrey Volkov <avolkov@mirantis.com>
2019-06-28 21:03:02 +0300

    Simple bash integration test

---
+++ tests/functional.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o errexit
+
+__DIR__="$(cd "$(dirname "${0}")"; echo $(pwd))"
+
+rm -rf /tmp/repos-test/
+mkdir -p /tmp/repos-test/orig/{r1,r2}
+
+cd /tmp/repos-test/orig/r1
+touch master-r1
+git init
+git add -A
+git commit -m "Auto-commit master-r1"
+git checkout -b b1
+touch b1-r1
+git add -A
+git commit -m "Auto-commit b1-r1"
+
+cd /tmp/repos-test/orig/r2
+touch master-r2
+git init
+git add -A
+git commit -m "Auto-commit master-r2"
+git checkout -b b1
+touch b1-r2
+git add -A
+git commit -m "Auto-commit b1-r2"
+
+cat << EOF > /tmp/repos-test/db.yaml
+repos:
+  - name: r1
+    path: /tmp/repos-test/r1
+    tags: []
+    group: openstack
+    remotes:
+      - name: origin
+        url: /tmp/repos-test/orig/r1
+      - name: r2
+        url: /tmp/repos-test/orig/r2
+    branches:
+      - name: master
+        remote: origin
+        refspec: master
+      - name: b1
+        remote: origin
+        refspec: b1
+      - name: r2-master
+        remote: r2
+        refspec: master
+      - name: r2-b1
+        remote: r2
+        refspec: b1
+
+  - name: r2
+    path: /tmp/repos-test/r2
+    tags: []
+    group: openstack
+    remotes:
+      - name: origin
+        url: /tmp/repos-test/orig/r2
+    branches:
+      - name: master
+        remote: origin
+        refspec: master
+      - name: b1
+        remote: origin
+        refspec: b1
+EOF
+
+REPOS_DB=/tmp/repos-test/db.yaml $__DIR__/../repos
+REPOS_DB=/tmp/repos-test/db.yaml $__DIR__/../repos

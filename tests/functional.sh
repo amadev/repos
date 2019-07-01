f8774e8b8fb9ea4947fd03c868a9070e83581cf6
Andrey Volkov <avolkov@mirantis.com>
2019-07-01 13:36:49 +0300

    CLI `repos add` command

--- tests/functional.sh
+++ tests/functional.sh
@@ -6,7 +6,7 @@ set -o errexit
 __DIR__="$(cd "$(dirname "${0}")"; echo $(pwd))"
 
 rm -rf /tmp/repos-test/
-mkdir -p /tmp/repos-test/orig/{r1,r2}
+mkdir -p /tmp/repos-test/orig/{r1,r2,r3}
 
 cd /tmp/repos-test/orig/r1
 touch master-r1
@@ -27,6 +27,16 @@ git checkout -b b1
 touch b1-r2
 git add -A
 git commit -m "Auto-commit b1-r2"
+
+cd /tmp/repos-test/orig/r3
+touch master-r3
+git init
+git add -A
+git commit -m "Auto-commit master-r3"
+git checkout -b b1
+touch b1-r3
+git add -A
+git commit -m "Auto-commit b1-r3"
 
 cat << EOF > /tmp/repos-test/db.yaml
 repos:
@@ -69,5 +79,8 @@ repos:
         refspec: b1
 EOF
 
-REPOS_DB=/tmp/repos-test/db.yaml $__DIR__/../repos
-REPOS_DB=/tmp/repos-test/db.yaml $__DIR__/../repos
+REPOS_DB=/tmp/repos-test/db.yaml $__DIR__/../repos add /tmp/repos-test/orig/r3 --path /tmp/repos-test/r3 -vv
+
+REPOS_DB=/tmp/repos-test/db.yaml $__DIR__/../repos pull -vv
+
+REPOS_DB=/tmp/repos-test/db.yaml $__DIR__/../repos pull -vv

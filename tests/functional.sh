1ea9615ea62813e91ff72c5fb66a4502a49d267a
Andrey Volkov <avolkov@mirantis.com>
2020-05-01 15:25:01 +0300

    Refactor functional tests

--- tests/functional.sh
+++ tests/functional.sh
@@ -1,43 +1,51 @@
 #!/usr/bin/env bash
 
-set -o pipefail
 set -o errexit
+
+echo
+echo "Testing functional.sh"
+echo "Use: REPOS_DEBUG=-vv bash -x tests/functional.sh for detailed output"
 
 __DIR__="$(cd "$(dirname "${0}")"; echo $(pwd))"
 
-rm -rf /tmp/repos-test/
-mkdir -p /tmp/repos-test/orig/{r1,r2,r3}
+function cleanup() {
+    rm -rf /tmp/repos-test/
+    mkdir -p /tmp/repos-test/orig/{r1,r2,r3}
+}
 
-cd /tmp/repos-test/orig/r1
-echo master-r1-content > master-r1
-git init
-git add -A
-git commit -m "Auto-commit master-r1"
-git checkout -b b1
-touch b1-r1
-git add -A
-git commit -m "Auto-commit b1-r1"
+function init_repos() {
+    cd /tmp/repos-test/orig/r1
+    echo master-r1-content > master-r1
+    git init -q
+    git add -A
+    git commit -m "Auto-commit master-r1" -q
+    git checkout -b b1 -q
+    touch b1-r1
+    git add -A
+    git commit -m "Auto-commit b1-r1" -q
 
-cd /tmp/repos-test/orig/r2
-echo master-r2-content > master-r2
-git init
-git add -A
-git commit -m "Auto-commit master-r2"
-git checkout -b b1
-touch b1-r2
-git add -A
-git commit -m "Auto-commit b1-r2"
+    cd /tmp/repos-test/orig/r2
+    echo master-r2-content > master-r2
+    git init -q
+    git add -A
+    git commit -m "Auto-commit master-r2" -q
+    git checkout -b b1 -q
+    touch b1-r2
+    git add -A
+    git commit -m "Auto-commit b1-r2" -q
 
-cd /tmp/repos-test/orig/r3
-touch master-r3
-git init
-git add -A
-git commit -m "Auto-commit master-r3"
-git checkout -b b1
-touch b1-r3
-git add -A
-git commit -m "Auto-commit b1-r3"
+    cd /tmp/repos-test/orig/r3
+    touch master-r3
+    git init -q
+    git add -A
+    git commit -m "Auto-commit master-r3" -q
+    git checkout -b b1 -q
+    touch b1-r3
+    git add -A
+    git commit -m "Auto-commit b1-r3" -q
+}
 
+function test_basic_opepations_with_initial_config() {
 cat << 'EOF' > /tmp/repos-test/config.yaml
 db: /tmp/repos-test/db.yaml
 groups: /tmp/repos-test/groups/
@@ -45,7 +53,6 @@ search_command: grep -HRin
 repos_default_directory: $HOME/src/
 log: /var/log/repos/repos.log
 EOF
-
 cat << EOF > /tmp/repos-test/db.yaml
 repos:
   - name: r1
@@ -88,25 +95,17 @@ groups:
   - name: openstack
     parent: root
 EOF
+    REPOS_CONFIG=/tmp/repos-test/config.yaml $__DIR__/../repos add /tmp/repos-test/orig/r3 \
+      --path /tmp/repos-test/r3 -t auto-commit --headers abc:def --name super-r3 $REPOS_DEBUG
+    REPOS_CONFIG=/tmp/repos-test/config.yaml $__DIR__/../repos pull $REPOS_DEBUG
+    REPOS_CONFIG=/tmp/repos-test/config.yaml $__DIR__/../repos pull $REPOS_DEBUG
+    REPOS_CONFIG=/tmp/repos-test/config.yaml $__DIR__/../repos list $REPOS_DEBUG
+    REPOS_CONFIG=/tmp/repos-test/config.yaml $__DIR__/../repos search master-r2-content $REPOS_DEBUG
+    REPOS_CONFIG=/tmp/repos-test/config.yaml $__DIR__/../repos sync $REPOS_DEBUG
+    REPOS_CONFIG=/tmp/repos-test/config.yaml $__DIR__/../repos add-group my-group --parent root $REPOS_DEBUG
+}
 
-REPOS_CONFIG=/tmp/repos-test/config.yaml $__DIR__/../repos add /tmp/repos-test/orig/r3 \
-  --path /tmp/repos-test/r3 -t auto-commit --headers abc:def -vv --name super-r3
-
-REPOS_CONFIG=/tmp/repos-test/config.yaml $__DIR__/../repos pull -vv
-
-REPOS_CONFIG=/tmp/repos-test/config.yaml $__DIR__/../repos pull -vv
-
-REPOS_CONFIG=/tmp/repos-test/config.yaml $__DIR__/../repos list -vv
-
-REPOS_CONFIG=/tmp/repos-test/config.yaml $__DIR__/../repos search master-r2-content -vv
-
-REPOS_CONFIG=/tmp/repos-test/config.yaml $__DIR__/../repos sync -vv
-
-REPOS_CONFIG=/tmp/repos-test/config.yaml $__DIR__/../repos add-group my-group --parent root -vv
-
-
-# test empty db
-
+function test_empty_db()  {
 cat << 'EOF' > /tmp/repos-test/config1.yaml
 db: /tmp/repos-test/db1.yaml
 groups: /tmp/repos-test/groups1/
@@ -114,13 +113,18 @@ search_command: grep -HRin
 repos_default_directory: $HOME/src/
 log: /var/log/repos/repos.log
 EOF
+    REPOS_CONFIG=/tmp/repos-test/config1.yaml $__DIR__/../repos list $REPOS_DEBUG
+    REPOS_CONFIG=/tmp/repos-test/config1.yaml $__DIR__/../repos add-group g1 $REPOS_DEBUG
+    REPOS_CONFIG=/tmp/repos-test/config1.yaml $__DIR__/../repos add-group g2 $REPOS_DEBUG
+    REPOS_CONFIG=/tmp/repos-test/config1.yaml $__DIR__/../repos add-group g1.1 --parent g1 $REPOS_DEBUG
+    REPOS_CONFIG=/tmp/repos-test/config1.yaml $__DIR__/../repos add-group g2.1 --parent g2 $REPOS_DEBUG
+    REPOS_CONFIG=/tmp/repos-test/config1.yaml $__DIR__/../repos add /tmp/repos-test/orig/r3 -g g2.1 $REPOS_DEBUG
+    REPOS_CONFIG=/tmp/repos-test/config1.yaml $__DIR__/../repos add /tmp/repos-test/orig/r3 | grep "already exists"
+    REPOS_CONFIG=/tmp/repos-test/config1.yaml $__DIR__/../repos list $REPOS_DEBUG
+}
 
-REPOS_CONFIG=/tmp/repos-test/config1.yaml $__DIR__/../repos add-group g1 -vv
-
-REPOS_CONFIG=/tmp/repos-test/config1.yaml $__DIR__/../repos add-group g2 -vv
-
-REPOS_CONFIG=/tmp/repos-test/config1.yaml $__DIR__/../repos add-group g1.1 --parent g1 -vv
-
-REPOS_CONFIG=/tmp/repos-test/config1.yaml $__DIR__/../repos add-group g2.1 --parent g2 -vv
-
-REPOS_CONFIG=/tmp/repos-test/config1.yaml $__DIR__/../repos add /tmp/repos-test/orig/r3 -vv
+cleanup
+init_repos
+test_basic_opepations_with_initial_config
+test_empty_db
+echo "ok"
